864f592ff518a08003dbd6b771b0386ea4cfe705
Rector <rectornethril@gmail.com>
2024-03-15 22:43:13 +0300

    Find max

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 355;
+int b = 45;
+int c = 1346;
+int d = 26;
+int e = 489;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}    
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
+
